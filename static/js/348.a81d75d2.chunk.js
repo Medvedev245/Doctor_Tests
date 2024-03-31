11c822ddf9a934997869161ae77f2e929224eeaf
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-03-31 06:42:21 +0000

    Deploying to gh-pages from @ Medvedev245/Doctor_Tests@cd2b0184d0f6b92115b725b309fd8502e4dccc4b 🚀

--- static/js/348.a81d75d2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkTest_for_doctors=self.webpackChunkTest_for_doctors||[]).push([[348],{348:(s,e,d)=>{d.r(e),d.d(e,{default:()=>c});var r=d(184);const c=()=>(0,r.jsxs)("div",{children:[(0,r.jsx)("div",{children:"Video"}),(0,r.jsx)("span",{children:"\u041f\u043e\u043a\u0430 \u043d\u0438\u0447\u0435\u0433\u043e \u043d\u0435\u0442 "})]})}}]);
-//# sourceMappingURL=348.a81d75d2.chunk.js.map
---
+++ static/js/648.e681a2b5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkTest_for_doctors=self.webpackChunkTest_for_doctors||[]).push([[648],{6648:(s,e,t)=>{t.r(e),t.d(e,{default:()=>o});t(184);const o=()=>{}}}]);
+//# sourceMappingURL=648.e681a2b5.chunk.js.map
