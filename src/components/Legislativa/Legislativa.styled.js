3d8650c100d4f45d18917cf8bb85045a701e0c81
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-04-14 20:45:41 +0200

    add paddini to TabComponent

--- src/components/Legislativa/Legislativa.styled.js
+++ src/components/Legislativa/Legislativa.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 //main container
 export const ItemContainer = styled.ul`
+  margin-top: 15px;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
