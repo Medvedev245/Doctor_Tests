4ba918b85cf19baec1e92f32f9d9986b25b11fe3
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-04-28 14:21:06 +0200

    tests 2117 zivny - adds

--- src/Files/Functions.jsx
+++ src/Files/Functions.jsx
@@ -86,221 +86,130 @@ const Functions = () => {
   //       });
   //   };
   const text = `
-  1. Krvácení při operaci v Retziově prostoru stavíme
-  a) aplikací fibrinogenu
-  b) kompresí horkou rouškou
-  c) opichy
-  d) elektrokoagulací
-  2. Při nálezu endometriózy u 471eté pacientky volíme
-  a) hysterektomii s oboustranou adnexektomii
-  b) oboustrannou adnexektomii s ponecháním dělohy
-  c) hysterektomii bez adnexektomie
-  d) hormonální léčbu agonisty GnRH
-  3. Při podezření na maligní tumor ovaria
-  a) odešleme pacientku na onkochirurgické pracoviště
-  b) na počátku operace odebereme výplachovou cytologu
-  c) se vždy snažíme exstirpovat tumor s neporušeným pouzdrem
-  d) provedeme oboustrannou adnexektomii
-  4. Chceme-li při laparotomii vypreparovat ureter, hledáme jej
-  a) v těsném sousedství a. uterina
-  b) na zadním listu plica lata
-  c) v retroperitoneu
-  d) při vstupu do močového měchýře
-  5. Nejčastější místo možného podvázání ureteru při  abdominální hysterektomii je
-  a) při opichu lig. sacrouterinum
-  b) při opichu lig. cardinale uteri
-  c) při opichu a. ovarica
-  d) při sutuře pochvy
-  6. Zjistíte-li v průběhu operace, že došlo к podvazu ureteru,
-  a) zavoláte urologa
-  b) uvolníte ligaturu
-  c) uložíte do ureteru stent
-  d) provedete nefrostomii
-  7. Dojde-li při operaci к poruše kontinuity ureteru,
-  a) okamžitě ukončíte gynekologickou operaci a zavolát  urologa
-  b) dokončíte gynekologickou operaci a s urologem ošetříte poranění
-  c) s urologem ošetříte poraněni a pak dokončíte  gynekologickou operaci
-  d) ošetříte poranění a zavoláte urologa
-  8. Boariho plastika je
-  a) náhrada distální části ureteru lalokem z močového měchýře
-  b) náhrada distální části ureteru kličkou tenkého střeva
-  c) plastika ureteru
-  d) historická metoda uzavření pochvy
-  9. Pozdní komplikací Burchovy kolpopexe bývá
-  a) stenóza ureteru
-  b) periostitis pubis
-  c) enterokéla
-  d) rektokéla
-  10. Když má pacientka týden po abdominální  hysterektomii teploty a bolesti v bedrech,
-  a) provedeme vylučovací urografii
-  b) zavoláme urologické konzilium
-  c) podáme antibiotika
-  d) provedeme ultrasonografii malé pánve a kalichopánvičkového systému ledvin
-11. Pacientka udává opožděnou menstruaci a koliko- vitou bolest v podbřišku. Diferenciálně diagnosI ticky lze uvažovat o
-a) torzi adnex
-b) abortus in cursu
-c) graviditas extrauterina
-d) adnexitidě
-12. Pacientka udává několik dní trvajícíjednostran-nou bolest v pravém podbřišku. Při palpačním I vyšetření hmatáme bolestivý tumor vedle dělohy, teplotu má 37,3 °C. Myslíme na
-a) torzi adnex
-b) inflamatomí adnextumor
-c) tubární abort
-d) periapendikulární infiltrát
-13. Ligatura ureteru se obvykle projeví
-a) tupou bolestí v bederní krajině
-b) teplotou event, zimnicí a třesavkou
-c) anurií
-d) retencí moči
-14. Intrafasciální abdominální hysterektomie má j výhodu
-a) v neporušeném cévním zásobení
-b) v neporušeném nervovém zásobení
-c) v neporušeném podpůrném aparátu
-d) ve snížení rizika poranění močovodu
-15. Ligaturu arteria iliaca int a hypogastrica provádíme
-a) podbifurkací a. iliaca com.
-b) ipodvazem spolu s lig. suspensorium ovarii
-c) přímo v bifurkaci a. iliaca com.
-d) v bázi postranního parametria
-16. Indikací к ligatuře arteria iliaca int a hypogastI rical je nejčastěji
-a) graviditas extrauterina tubaria rupta Ktenorrhagn
-b) ?? tenorrhagn ??
-c) nezvladatelné porodnické krvácení
-d) krvácení z nekrotického karcinomu colli uteri
-17. Při děložní myomatóze indikujeme
-a) ve vhodných případech embolizaci uterinních arterií
-b) vaginální nebo abdominální hysterektomii
-c) laparoskopicky asistovanou vaginální hysterektomii
-d) Вenukleaci myomů (při příznivém nálezu)
-18. Extrauterinní gravidita může proběhnout
-a) asymptomaticky a dojde к její resorpci
-b) nsymptomalicky a dosáhnout velikosti zralého plodu
-c) s výraznými klinickými projevy
-d) současně s graviditou intrauterinní
-19. Nejnebezpečnější lokalizací extrauterinní gravidity je gravidita
-a) intraabdominální
-b) ampulámí
-c) ovariální
-d) v děložním rohu
-20. Z níže uvedených stavů jsou absolutní indikací к operaci
-a) obstrukčm ileus
-b) hemoperitoneum
-c) ovulační krize
-d) endotoxinový šok
-21. Prevence tromboembolické choroby spočívá
-a) v časné pooperační mobilizaci
-b) v miniheparinizaci
-c) v šetrné operační technice
-d) v předoperační infúzi 5000 j. heparinu
-22. Za dezinfekci operačního pole zodpovídá
-a) operatér
-b) staniční sestra operačního sálu
-c) instrumentářka u operace
-d) staniční sestra na předoperačním oddělení
-23. Po embolizaci uterinních arterií pro děložní myomatózu bývá obvykle
-a) krvácení z rodidel
-b) bolest v podbřišku
-c) subfebrilie
+  1. mendelenburgova poloha způsobuje
+  a) pokles vitální kapacity plic, pokles plicní compliance poddajnost)
+  b)kvýšení návratu krve z dolních končetin s nebezpečím řetížení srdce u kardiaček
+  c)bvýšené nebezpečí vzduchové embolie při operačním poli nad úrovní procházející srdcem výšený nitrobřišní tlak a zvýšené nebezpečí
+  d) egurgitace a aspirace
+  2. Gynekologická poloha
+  a) cvyšuje nitrobřišní tlak, a tím omezuje žilní návrat
+  b) «zvyšuje nitrobřišní tlak, a tím stoupá i nebezpečí egurgitace
+  c) nemá vliv na nitrobřišní tlak a frekvenci regurgitace (Ijomezuje pohyb bránice a ohrožuje dechovou
+  d)isuficiencí hlavně obézní pacientky a pacientky s obnikční bronchopulmonální chorobou
+  3. při TUR syndromu může dojít
+  a) kpřetížení oběhu izotonickou tekutinou
+  b) khypoosmolámímu syndromu (otrava vodou) při užití
+  c) beziontové výplachové tekutiny
+  d) suficiencí hlavně obézní pacientky a pacientky s obnikční bronchopulmonální chorobou
+
+  4. Klinické příznaky TUR syndromu jsou
+  a) k Hhypertenze, bradykardie
+  b) kHhypotenze a porucha střevní peristaltiky
+  c) zmatenost, nauzea, křeče, kóma
+  d) dyspnoe, plicní otok
+  5. Varovné známky před křečemi při toxické reakci  při předávkování lokálními anestetiky jsou
+  a) tupý pocit na jazyku a rtech
+  b) kovová pachuť v ústech
+  c) ospalost, opilost, smazaná řeč
+  d) svalový třes
+  6. Nejčastější pozdní komplikace po subarachnoidální anestezii jsou
+  a) snížená hybnost dolních končetin
+  b) retence moči a postpunkční bolesti hlavy
+  c) poruchy sluchu a vizu
+  d) poruchy motility trávicího ústrojí
+  7. Pro vzácnou komplikaci epidurální nebo subarachnoidální anestezie pro epidurální hematom svědčí
+  a) bolesti v zádech nebo dolních končetinách nebo  slabost či obrna dolních končetin
+  b) bolesti na hrudi, palpitace, hypotenze
+  c) svalový třes, smazaná řeč
+  d) porucha pasáže trávicím ústrojím trvající asil2 hodin
+  8. Podezření na dehydrataci u pacienta máme  u následujících stavů
+  a) průjem, střevní příprava
+  b) zvracení, střevní píštěle
+  c) teplota
+  d) hyperglykémie
+  9. Ke klinickému obrazu dehydratace patří
+  a) apatie, poruchy vědomí
+  b) suché sliznice
+  c) tachykardie, hypotenze
+  d) oligurie
+  10. Hypokalémie vede
+  a) к poruchám srdečního rytmu
+  b) к atonii střevní a žaludeční
+  c) ke svalové slabosti
+  d) к hypoventilaci až zástavě dechu
+  11. Konzervativní léčba akutní hyperkalémie  zahrnuje
+  a) infúzní léčbu koncentrovaného roztoku glukózy a inzulínu
+  b) intravenózní podání iontů kalcia
+  c) alkalizaci vnitřního prostředí bikarbonátem
+  d) léčbu kličkovými diuretiky (Furosemid)
+  12. Varovné příznaky hemolytické potransfúzní reakce jsou
+  a) pálení a bolest v průběhu žíly, do které je aplikováno
+  b) sevření na hrudi, neklid, nevolnost, oprese, bolest v bedrech
+  c) třesavka, teplota, studený pot, bolesti hlavy
+  d) tachykardie, hypotenze, tachypnoe
+13. Léčba potransfúzní hemolytické reakce zahrnuje
+a) přerušení transfúze
+b) volumoterapii a vazopresory
+c) kortikoidy ve vysokých dávkách
+d) forsírovanou diurézu s alkalizací moči
+14. Pooperačně špatně tlumená bolest může vést
+a) к sníženému dechovému objemu, snížené vitální kapacitě, špatné toaletě dýchacích cest a atelektázám
+b) к hypertenzi a tachykardii a zvýšené srdeční práci a ischémii myokardu
+c) к poruše motility trávicího ústrojí s nauzeou a zvracením a subileózním stavem
+d) к retenci moči
+15. Parenterálně podaný pethidin (Dolsin)
+a) má střední délku účinku 2—4 hodin
+b) je vhodný ke kontinuálnímu podání
+c) při opakovaném podání se kumuluje jeho neurotoxický metabolit, který může vést к halucinacím, myoklonu a křečím
+d) má střední délku účinku 6-8 hodin
+16. Známky zástavy oběhu jsou
+a) nehmatný tep na velkých cévách (a. carotis, a. femoralis)
+b) bezvědomí
+c) lapavé dechy či bezdeší
+d) mrtvolný vzhled pacienta
+17. Pooperační drobné atelektázy léčíme
+a) farmakologicky (bronchodilatanciemi, mukoiytiky, inhalacemi)
+b) kvalitní analgezií
+c) rehabilitací dechovou (hluboké vdechy, dýchám proti odporu, poklepovou masáží)
+d) proto, abychom zabránili vzniku bronchopneumonie
+18. Nedostatečná pooperační analgezie způsobuje
+a) neklid, nespolupráci, nespavost
+b) nedostatečnou ventilaci, neschopnost odkašlat, hypoxémii
+c) menší útlum a časnější rehabilitaci pacientky
+d) tachykardii, hypertenzi, asymptomatickou ischémii myokardu
+19. Drenáž močového měchýře u operovaných žen se provádí
+a) transuretrálně Foleyovým permanentním katétrem s balónkem
+b) transuretrálně polyetylenovou cévkou bez balónku
+c) suprapubicky epicystostomií
+d) cystoskopicky
+20. Komplikací suprapubické drenáže je
+a) intravezikální krvácení
+b) infekce místa vpichu
+c) poranění střeva
 d) retence moči
-24. Z uvedených operací má největší riziko podvázání ureteru
-a) vaginální hysterektomie
-b) abdominální hysterektomie pro endometriózu ovarií
-c) pánevní lymfadenektomie
-d) kolpopexe podle Burche
-25. Při peroperačně zjištěné akutní apendicitidě gynekolog
-a) z forenzních důvodů vždy přivolá chirurga
-b) ukončí gynekologickou operaci a pacientku přeloží na chirurgické oddělení
-c) dokončí gynekologickou operaci a do propouštěcí zprávy uvede doporučení к provedení apendektomie
-d) může provést sám apendektomii -tuto operaci musí ovládat
-26. Z níže uvedených stavů jsou absolutní indikací к operaci
-a) bolesti v podbřišku nejasné etiologie
-b) pelveoperitonitida
-c) peritonitida
-d) hemoperitoneum
-27. Zmnožení a rozšíření žil na břiše, tzv. caput Medusae, vídáme při
-a) masivním hemoperitoneu
-b) cirhóze jater
-c) těžké těhotenské hepatopatii
-d) karcinomu ovaria
-28. Monopolární elektrochirurgické výkony smíme provádět při operaci
-a) pacientek se zdravým kardiovaskulárním aparátem
-b) pacientek s kardiostimulátorem
-c) pacientek s epilepsií
-d) pacientek na řízené ventilaci
-29. Příměs adrenalinu к lokálnímu znecitlivění je kontraindikovaná
-a) u diabetů
-b) u toxické strumy
-c) u těhotných žen
-d) u ranné infekce
-30. Autotransfúze není indikována při operačním řešení
-a) u onkologických pacientek
-b) u pacientek s těžší anémií
-c) u děložní myomatózy
-d) u těhotných žen
-31. Sutura pokračujícím stehem je vhodná
-a) к šití střeva
-b) к šití močového měchýře
-c) к šití cévní stěny
-d) к šití fascie
-32. Nevstřebatelný šicí materiál je
-a) silon
-b) ethilon
-c) ethibond
-d) nurolon
-33. Propustnost kapilár ovlivňují
-a) vitamin К
-b) trasylol
-c) vitamin C
-d) kortikosteroidy
-34. Kolostomii provádíme při gynekologické operaci
-a) u pacientek s karcinomem ovaria s inoperabilním tumorem v malé pánvi
-b) při známkách nízkého ileu
-c) při známkách vysokého ileu
-d) při zjištění tumoru střeva
-35. Vstřebatelný šicí materiál se používá na suturu
-a) fascie
-b) střeva
-c) dělohy
-d) kůže
-36. Transfúze jedné transfúzníjednotky (TU) erymasy zvýší hematokrit asi o
-a) 4%
-b) 8%
-c) 10%
-d) 12%
-37. Při radikální operaci pro karcinom ovarií provádíme apendektomii
-a) vždy, kdy je to možné
-b) jen při makroskopickém postižení apendixu
-c) jen u mladších pacientek v dobrém celkovém stavu
-d) provádí ji pouze chirurg
-38. Při nádorovém postižení sentinelové uzliny u karcinomu děložního hrdla
-a) ukončíme operaci a indikujeme radioterapii
-b) provedeme radikální hysterektomii s pánevní lymfadenektomií
-c) provedeme radikální hysterektomii s pánevní a paraaortální lymfadenektomií
-d) provedeme radikální hysterektomii s pánevní, paraaortální a ingvinální lymfadenektomií
-39. К radiklání vulvektomii patří
-a) odstranění vulvy i s pokožím až na fascii včetně perinea až к řitnímu svěrači a pánevní lymfadenektomie
-b) odstranění ingvinálních uzlin
-c) odstranění uzlin z fossa femoralis
-d) odstranění vulvy i s podkožím až na fascii včetně perinea až řitnímu svěrači
-40. Při nádorovém postižení pánevních uzlin při karcinomu děložního těla
-a) ukončíme operaci a indikujeme radioterapii
-b) provedeme rozšířenou hysterektomii s pánevní lymfadenektomií
-c) provedeme abdominální hysterektomii s oboustrannou adnexektomií, pánevní a paraaortální lymfadenektomii
-d) provedeme rozšířenou hysterekotomii s pánevní  a paraaorální lymfadenektomii
-41. Při invazi karcinomu do děložního těla a do děložního hrdla indikujeme
-a) primární radioterapii
-b) rozšířenou hysterektromii s pánevní lymfadenektomií
-c) extrafasciální hysterekotmii s oboustrannou adnexektomií
-d) primární radioterapii jen u pacientek neschopných operace
-42. Při radikální operaci pro karcinom ovaria je indikována omentektomie
-a) infrakolická a suprakolická vždy, kdy je to možné
-b) jen při prokázaném nádorovém postižení omenta v peroperační histologií
-c) jen infrakolická, u případů bez prokázaného nádorového postižení v peroperační histologii
-d) jen při jeho makroskopickém postižení
+21. Neodkladnou resuscitaci ve fakultní nemocnici
+a) zahajujeme v případě nutnosti vždy
+b) nezahajujeme při hypovolemickém šoku a intoxikaci
+c) zahajujeme přivoláním anesteziologického týmu
+d) nezahajujeme u pacientů s prokázaným terminálním stadiem nevyléčitelného onemocnění nebo jasnými známkami smrti (posmrtné skvrny, ztuhlost)
+22. Při asystolii u dospělého člověka je indikováno podání
+a) Mesocainu
+b) kalcia
+c) adrenalinu
+d) noradrenalinu
+23. První deflbrilační výboj při fibrilaci komor nebo komorové tachykardii provedeme energií při monofázické defibrilaci
+a) 12 J
+b) 400J
+c) 200 J
+d) 100 J
+
 
 `;
 
   const answers = `
- GOTOWO
+  1a,b,c,d 2b,d 3a,b,d 4a,c,d 5a,b,c,d 6b 7a 8a,b,c,d 9a,b,c,d
+  10a,b,c 11a,b,c,d 12a,b,c,d 13a,b,c,d 14a,b,c,d 15a,c 16a,b,c,d 17a,b,c,d 18a,b,d 19a,b,c
+  20a,b,c 21d 22c 23c
   `;
   const correctNewAnswers = answers.split(' ');
   //   console.log(correctNewAnswers);
@@ -347,7 +256,9 @@ d) jen při jeho makroskopickém postižení
         question: `${questionNumber + question}`,
         answers: newAnswers,
         book: ['Živný'], // Пример значения для поля "book"
-        topic: ['13. Gynekologická operativa. II. ČÁST POSTGRADUÁLNÍ'],
+        topic: [
+          '15. Anestezie, pooperační péče a základy resuscitace. II. ČÁST POSTGRADUÁLNÍ',
+        ],
         correctAnswers: myCorrectNewAnswers, // Массив с правильными ответами
       });
     }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -110,7 +110,7 @@ const Home = () => {
           >
             <option value="All">All</option>
             <option value="Klener">Klener</option>
-            <option value="Živný">Živný(1-1615)</option>
+            <option value="Živný">Živný(1-2117)</option>
             <option value="Legislativa_Báze_2023">Legislativa_Báze_2023</option>
             <option value="Medicine_2023">Medicine_2023</option>
             <option value="My_Base">My Base</option>
