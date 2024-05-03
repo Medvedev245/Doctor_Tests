b6e5c2383a2655367f214f3224d744593dc1c46f
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-05-03 07:20:44 +0200

    change height end width progresbar

--- src/components/Progressbar/Progressbar.styled.js
+++ src/components/Progressbar/Progressbar.styled.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const PersentContainer = styled.div`
-  width: 100px;
-  height: 100px;
+  width: 200px;
+  height: 200px;
   @media screen and (max-width: 1200px) {
   }
   // Mobil
   @media screen and (max-width: 767px) {
-    width: 50px;
-    height: 50px;
+    width: 100px;
+    height: 100px;
   }
 `;
