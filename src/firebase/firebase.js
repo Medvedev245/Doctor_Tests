7476f301b45bcf53a07fa6e9394f67be588be065
Ihor_Koniaha <konyaga.igor10@gmail.com>
2024-05-10 14:12:07 +0300

    create  and config firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCqrMZSEkdPXvR3effc0qbe26Lg9uUHQdY',
-  authDomain: 'medicine-tests-db.firebaseapp.com',
-  projectId: 'medicine-tests-db',
-  storageBucket: 'medicine-tests-db.appspot.com',
-  messagingSenderId: '818702799015',
-  appId: '1:818702799015:web:d5b3c44de02458cb7bcb82',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
