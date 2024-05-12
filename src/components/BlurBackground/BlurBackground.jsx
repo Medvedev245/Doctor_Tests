d970efbca6ea8a37ce9d5305837d6a9f368af0be
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-05-12 20:01:38 +0200

    the second component was redesigned as typescript component

--- src/components/BlurBackground/BlurBackground.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Container } from './BlurBackground.styled';
-
-export const BlurBackground = ({ onClick }) => {
-  return <Container onClick={onClick}></Container>;
-};
