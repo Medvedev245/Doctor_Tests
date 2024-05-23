11803433ccbded6a2a13036f86cb8e8c2707225c
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-05-23 16:54:42 +0200

    change cannel component to TypeScript

--- src/components/Canals/Canals.tsx
+++ src/components/Canals/Canals.tsx
@@ -3,6 +3,12 @@ import VideoCanals from '../../Files/VideoCanals.json';
 import { CanalLisl, CanalElement, MainText, SmallText } from './Canals.styled';
 import icon from '../../Files/img/youtube-svgrepo-com.png';
 
+interface VideoChannel {
+  link: string;
+  channel: string;
+  smallDescr: string;
+}
+
 const Canals = () => {
   // console.log(VideoCanals);
   return (
