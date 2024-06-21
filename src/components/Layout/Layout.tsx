705b19e966cf040a63718fedfc36e5cfe1bbf398
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-06-21 06:25:08 +0200

    add layaut tsx component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { Container, MainContainer } from './Layout.styled';
 import { Footer } from 'components/Footer/Footer';
 
-//----Удалить
-
-export const Layout = () => {
+export const Layout: React.FC = () => {
   return (
     <Container>
       <Header />
