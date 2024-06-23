adee728f23f8017ee48e35fed0541c44daa92506
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-06-23 18:28:15 +0200

    refactoring component LoadMore from js to ts

--- src/components/LoadMore/LoadMore.tsx
+++ src/components/LoadMore/LoadMore.tsx
@@ -1,11 +1,27 @@
 import { BtnContainer } from './LoadMore.styled';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import { Interface } from 'readline';
 
-const LoadMore = ({ currentPage, testsPerPage, totalTests, onPageChange }) => {
+interface LoadMoreProps {
+  currentPage: number;
+  testsPerPage: number;
+  totalTests: number;
+  onPageChange: (event: React.ChangeEvent<unknown>, page: number) => void;
+}
+
+const LoadMore: React.FC<LoadMoreProps> = ({
+  currentPage,
+  testsPerPage,
+  totalTests,
+  onPageChange,
+}) => {
   const numPages = Math.ceil(totalTests / testsPerPage);
 
-  const handlePageChange = (event, newPage) => {
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    newPage: number
+  ) => {
     onPageChange(event, newPage);
     window.scrollTo({ top: 0, behavior: 'smooth' }); // Прокрутка страницы в начало
   };
