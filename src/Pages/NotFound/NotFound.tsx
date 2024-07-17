de3e20c27b9701560b9a404b6f21685791bad27e
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-07-17 17:00:51 +0200

    change NotFound component from jsx to tsx

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 import React, { FC } from 'react';
 
-const NotFound.FC = () => {
-  <div>NotFound</div>;
-  <Link to="/">to Home Page</Link>;
+const NotFound: FC = () => {
+  return (
+    <div>
+      NotFound
+      <Link to="/">to Home Page</Link>
+    </div>
+  );
 };
 
 export default NotFound;
